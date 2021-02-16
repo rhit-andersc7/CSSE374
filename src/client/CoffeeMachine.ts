d72a08d903bc9ab5b49b58032272cc638353bf97
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-02-16 18:29:17 -0500

    Write tests for coffee machine

--- src/client/CoffeeMachine.ts
+++ src/client/CoffeeMachine.ts
@@ -1,12 +1,15 @@
 import Command from "../model/Command";
+import Condiment from "../model/Condiment";
 
 export default class CoffeeMachine {
 	private static machines: CoffeeMachine[] = [];
 
 	public id: number;
+	public unadded: Set<Condiment>;
 
 	constructor() {
 		this.id = CoffeeMachine.machines.push(this);
+		this.unadded = new Set();
 	}
 
 	serve(): void {
@@ -18,7 +21,21 @@ export default class CoffeeMachine {
 		if (!["Expresso", "Americano"].includes(command.drink)) {
 			throw new Error("Unable to brew drink");
 		}
+
+		for (let condiment of command.options) {
+			this.unadded.add(condiment);
+		}
+
 		return true;
+	}
+
+	getUnadded(): string {
+		let condiments: string[] = [];
+		for (let condiment of this.unadded) {
+			condiments.push(condiment.name + ": " + condiment.quantity);
+		}
+
+		return condiments.join(", ");
 	}
 
 	public static getMachine(id: number): CoffeeMachine {
--- test/machine.test.ts
+++ test/machine.test.ts
@@ -1,5 +1,21 @@
+import CoffeeMachine from "../src/client/CoffeeMachine";
+import { commandFromOrder } from "../src/model/Command";
+import Order from "../src/model/Order";
+
 describe("Coffee machine display", () => {
 	it("displays unadded condiments", () => {
-		// TODO(cooper-anderson): Implement
+		let machine = new CoffeeMachine();
+		let request: Order = {
+			id: 1,
+			address: {street: "", zip: ""},
+			drink: "Expresso",
+			condiments: [
+				{name: "cream", quantity: 150},
+				{name: "ice", quantity: 3}
+			]
+		};
+
+		machine.brew(commandFromOrder(request, 0));
+		expect(machine.getUnadded()).toBe("cream: 150, ice: 3");
 	});
 });
